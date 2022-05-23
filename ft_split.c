0c65452c8b150320535d67e571f9c708a4601185
rgrollma <rgrollma@student.42wolfsburg.de>
2022-05-23 17:40:36 +0200

    Getting closer to finish.

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,42 @@
 /*   By: rgrollma <rgrollma@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 13:15:38 by rgrollma          #+#    #+#             */
-/*   Updated: 2022/05/23 13:26:12 by rgrollma         ###   ########.fr       */
+/*   Updated: 2022/05/23 17:39:37 by rgrollma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+int	count_keep(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	while (s[i] && s[i] == c)
+		i++;
+	if (s[i] == '\0')
+		return (0);
+	count = 1;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != '\0')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	int		j;
+	int		begin;
+	char	**array;
+
+	i = 0;
+	j = 0;
 	
 }
 
